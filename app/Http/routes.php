8ce2a06921ce86c708b0366356504a21eb0288b2
KOSIT AROMSAVA <kosit.arom@gmail.com>
2015-12-06 14:26:37 +0700

    COMMIT FROM SERVER010

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -2,7 +2,7 @@
 
 /*
 |--------------------------------------------------------------------------
-| Application Routes TEST009
+| Application Routes TEST010
 |--------------------------------------------------------------------------
 |
 | Here is where you can register all of the routes for an application.
