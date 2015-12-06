3d5766962d86a58fae22b345a6b966b24e6c8eb1
nong053 <kosit.arom@gmail.com>
2015-12-06 14:55:58 +0700

    test13

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -4,7 +4,7 @@
 |--------------------------------------------------------------------------
 | Application Routes TEST013
 |--------------------------------------------------------------------------
-|
+|test13
 | Here is where you can register all of the routes for an application.
 | It's a breeze. Simply tell Laravel the URIs it should respond to
 | and give it the controller to call when that URI is requested.
