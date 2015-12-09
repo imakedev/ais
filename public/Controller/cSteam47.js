ce087171aa8c60fb0963ac077e588e43de321d28
nong053 <kosit.arom@gmail.com>
2015-12-09 19:29:31 +0700

    1

--- public/Controller/cSteam47.js
+++ public/Controller/cSteam47.js
@@ -1,52 +1,87 @@
-var mainSteam47Fn = function(paramDate){
-
+var createHtmlForGridEventFn = function(data){
+	var htmlTableGridEvent="";
+	htmlTableGridEvent +="<table id=\"gridEventList\">";
+    htmlTableGridEvent +="<colgroup>";
+        htmlTableGridEvent +="<col style=\"width:100%\"/>";
+    htmlTableGridEvent +="</colgroup>";
+    htmlTableGridEvent +="<thead>";
+       htmlTableGridEvent +=" <tr>";
+            htmlTableGridEvent +="<th data-field=\"field1\"><b>Event</b></th>";
+        htmlTableGridEvent +="</tr>";
+  htmlTableGridEvent +="  </thead>";
+    htmlTableGridEvent +="<tbody id='gridSteam47DataArea'>";
+    
+    	$.each(data,function(index,indexEntry){
+    		//alert(indexEntry['sys_date']);
+    		//alert(indexEntry['ois_event']);
+    		
+    		 htmlTableGridEvent +="<tr>";
+    		 	htmlTableGridEvent +="<td>"+indexEntry['ois_event']+"</td>";
+             htmlTableGridEvent +=" </tr>";
+    		
+    	});
+       
+     
+   htmlTableGridEvent +=" </tbody>";
+htmlTableGridEvent +="</table>";
+	$("#gridSteam47Area").html(htmlTableGridEvent);
+	//$("#gridEventList")
+};
 var logicPointSteam47 = {
+		
+		setDateTime(EvTime){
+			
+			$("#disPlayDateTimeArea").html(EvTime);	
+			
+		},
 		point1(paramDataPoint){
 			
-			$("#point1").html("<font color='red'>1,</font>"+paramDataPoint);	
+			$("#point1").html("<font color='red'>1,</font>"+parseFloat(paramDataPoint).toFixed(2));	
 			
 		
 		},point2(paramDataPoint){
 		
-			$("#point2").html("<font color='red'>2,</font>"+paramDataPoint);	
+			$("#point2").html("<font color='red'>2,</font>"+parseFloat(paramDataPoint).toFixed(2));	
 			
 		},point3(paramDataPoint){
 			
-			$("#point3").html("<font color='red'>3,</font>"+paramDataPoint);	
+			$("#point3").html("<font color='red'>3,</font>"+parseFloat(paramDataPoint).toFixed(2));	
 			
 		},point4(paramDataPoint){
 			
-			$("#point4").html("<font color='red'>4,</font>"+paramDataPoint);	
+			$("#point4").html("<font color='red'>4,</font>"+parseFloat(paramDataPoint).toFixed(2));	
 			
 		},point5(paramDataPoint){
 			
-			$("#point5").html("<font color='red'>5,</font>"+paramDataPoint);	
+			$("#point5").html("<font color='red'>5,</font>"+parseFloat(paramDataPoint).toFixed(2));	
 			
 		},point6(paramDataPoint){
 			
-			$("#point6").html("<font color='red'>6,</font>"+paramDataPoint);	
+			$("#point6").html("<font color='red'>6,</font>"+parseFloat(paramDataPoint).toFixed(2));	
 			
 		},point7(paramDataPoint){
 			
-			$("#point7").html("<font color='red'>7,</font>"+paramDataPoint);	
+			$("#point7").html("<font color='red'>7,</font>"+parseFloat(paramDataPoint).toFixed(2));	
 			
 		},point8(paramDataPoint){
 			
-			$("#point8").html("<font color='red'>8,</font>"+paramDataPoint);	
+			$("#point8").html("<font color='red'>8,</font>"+parseFloat(paramDataPoint).toFixed(2));	
 			
 			
 		},point9(paramDataPoint){
 			
-			$("#point9").html("<font color='red'>9,</font>"+paramDataPoint);	
+			$("#point9").html("<font color='red'>9,</font>"+parseFloat(paramDataPoint).toFixed(2));	
 			
 		},point10(paramDataPoint){
 			
-			$("#point10").html("<font color='red'>10,</font>"+paramDataPoint);	
+			$("#point10").html("<font color='red'>10,</font>"+parseFloat(paramDataPoint).toFixed(2));	
 			
 		}
 }
-function setDataOnDashboardStream47(data){
 
+function setDataOnDashboardStream47(data){
+	
+	logicPointSteam47.setDateTime("<center><b>ข้อมูลวันที่ "+convertDateHisTh(data['EvTime'])+"</b></center>");
 	logicPointSteam47.point1(data['D1']);
 	logicPointSteam47.point2(data['D2']);
 	logicPointSteam47.point3(data['D3']);
@@ -61,38 +96,180 @@ function setDataOnDashboardStream47(data){
 	
 	
 }	
-	
 var stream47={
-	//alert("hello jquery");
-	disPlayDateTimeFn(paramDate){
-		
-		$("#disPlayDateTimeArea").html("ข้อมูลวันที่  "+convertDateTh(paramDate+" 00:00:00"));
+		//alert("hello jquery");
+		disPlayDateTimeFn(paramDate){
+			
+			$("#disPlayDateTimeArea").html("<center><b>ข้อมูลวันที่  "+convertDateHisTh(paramDate+" 00:00:00")+"</b></center>");
 
-	},
-	
-	createDataPCVSteam47Fn(){
+		},
+		readDataEventPCVSteam47Fn(paramPcv,paramUnit,paramEmpId){
+			 var jsonFilter = new Array();
+			$.ajax({
+				url:"/ais/processView/readDataEventPCVSteam47/"+paramPcv+"/"+paramUnit+"/"+paramEmpId+"",
+				type:"get",
+				dataType:"json",
+				async:false,
+				success:function(data){
+					console.log(data);
+					
+					var paramSpanTime = (parseInt($("#paramSpanTimeEmbed").val())/2);
+					var paramFromDate2=intervalDelFn($("#paramToDateEmbed").val(),'minute',paramSpanTime);
+					var paramToDate2=intervalAddFn($("#paramToDateEmbed").val(),'minute',paramSpanTime);
+					/*
+					alert(paramSpanTime);
+					alert(paramFromDate2);
+					alert(paramToDate2);
+					*/
+					$.each(data,function(index,indexEntry){
+						
+						if((toTimestamp(indexEntry['sys_date'])>=toTimestamp(paramFromDate2)) && (toTimestamp(indexEntry['sys_date'])<=toTimestamp(paramToDate2))) {
+							jsonFilter.push(indexEntry);
+							//console.log(indexEntry);
+							
+						}
+						
+					});
+					console.log("jsonFilter");
+					console.log(jsonFilter);
+					
+					//createHtmlForGridEventFn(data);
+					
+				}
+			});
+		},
+		readDataPCVSteam47Fn(paramPcv,paramUnit,paramEmpId,paramIndexDate){
+			//alert(paramIndexDate);
+			var jsonFilter = new Array();
+			/*
+			alert(paramPcv);
+			alert(paramUnit);
+			alert(parmEmpId);
+			alert(paramIndexDate);
+			*/
+			// TEST->http://localhost:9999/ais/processView/readDataPCVSteam47/steam47/4/3
+			$.ajax({
+				url:"/ais/processView/readDataPCVSteam47/"+paramPcv+"/"+paramUnit+"/"+paramEmpId+"",
+				type:"get",
+				dataType:"json",
+				async:false,
+				success:function(data){
+					
+				if((paramIndexDate!="") && (paramIndexDate!=undefined)){
+					//console.log("--1");
+					//console.log(parseInt(paramIndexDate));
+					var paramMax=data.length;
+					var paramStart=data.length;
+					//console.log("--2"+data.length);
+					$.each(data,function(index,indexEntry){
+							//console.log(index+"="+(parseInt(paramIndexDate)-1));
+						if(index==(parseInt(paramIndexDate)-1)){
+							//slideFucusExpressFn(paramIndexDate,paramMax);
+							//console.log("--2");
+							console.log(indexEntry);
+							setDataOnDashboardStream47(indexEntry);
+							
+						}
+					});
+				}else{
+					
+					var paramMax=data.length;
+					var paramStart=data.length;
+					var lastObject=data;
+					lastObject = lastObject.pop();
+					slideFucusExpressFn(paramStart,paramMax);
+					setDataOnDashboardStream47(lastObject);
+				}
+					/*
+					$.each(data,function(index,indexEntry){
+						
+						if(toTimestamp(indexEntry['EvTime'])==toTimestamp(lastObject['EvTime'])) {
+							jsonFilter.push(indexEntry);
+							//console.log(indexEntry);
+							
+						}
+						
+					});
+					*/
+					
+				}
+			 //console.log(jsonFilter);
+			});
+
+		},
+		createDataPCVSteam47Fn(paramPcv,paramUnit,paramEmpId,paramFromDate,paramToDate){
+			/*
+			alert(paramPcv);
+			alert(paramUnit);
+			alert(parmEmpId);
+			alert(paramFromDate);
+			alert(paramToDate);
+			*/
+			
+			//{paramPCV}/{paramUnit}/{parmEmpId}/{paramFromDate}/{paramToDate}
+			
+			
+			$.ajax({
+				url:"/ais/processView/createDataPCVSteam47/"+paramPcv+"/"+paramUnit+"/"+paramEmpId+"/"+paramFromDate+"/"+paramToDate+"",
+				type:"get",
+				dataType:"json",
+				async:false,
+				success:function(data){
+					
+					if(data=='createJsonSuccess'){
+						
+						stream47.readDataPCVSteam47Fn(paramPcv,paramUnit,paramEmpId,'');
+						
+					}
+					console.log(data);
+					
+					//setDataOnDashboardStream47(data[0]);
+				}
+			});
+			
+			
+		},
+		createDataEventPCVSteam47Fn(paramPcv,paramUnit,paramEmpId,paramFromDate,paramToDate){
+			
+			
+			//{paramPCV}/{paramUnit}/{parmEmpId}/{paramFromDate}/{paramToDate}
+			
+			
+			$.ajax({
+				url:"/ais/processView/createDataEventPCVSteam47/"+paramPcv+"/"+paramUnit+"/"+paramEmpId+"/"+paramFromDate+"/"+paramToDate+"",
+				type:"get",
+				dataType:"json",
+				async:false,
+				success:function(data){
+					
+					if(data=='createJsonSuccess'){
+						
+						//stream47.readDataPCVSteam47Fn(paramPcv,paramUnit,paramEmpId,'');
+						 stream47.readDataEventPCVSteam47Fn(paramPcv,paramUnit,paramEmpId);
+						
+					}
+					console.log(data);
+					
+					//setDataOnDashboardStream47(data[0]);
+				}
+			});
+			
+			
+		}
 		
-		//alert();
-		//{paramPCV}/{paramUnit}/{parmEmpId}/{paramFromDate}/{paramToDate}
 		
-		/*
-		$.ajax({
-			url:"/ais/processView/createDataPCVSteam47/4/2014-05-01 00:00:00/2014-05-01 01:10:00",
-			type:"get",
-			dataType:"json",
-			async:false,
-			success:function(data){
-				setDataOnDashboardStream47(data[0]);
-			}
-		});
-		*/
 		
-	}
-	
-	
-	
-	
-}
-stream47.createDataPCVSteam47Fn();
-stream47.disPlayDateTimeFn(paramDate);
+		
+	}	
+var mainSteam47Fn = function(paramPcv,paramUnit,parmEmpId,paramFromDate,paramToDate){
+
+//test read processViewJson-steam47-4-3.txt
+stream47.createDataPCVSteam47Fn(paramPcv,paramUnit,parmEmpId,paramFromDate,paramToDate);	
+
+
+stream47.createDataEventPCVSteam47Fn(paramPcv,paramUnit,parmEmpId,paramFromDate,paramToDate);
+
+
+stream47.disPlayDateTimeFn(paramToDate);
+
 }
